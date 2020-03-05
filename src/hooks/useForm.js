5539db67341eed8c1115f66a96c554911651e3f3
VsevolodKurochka <seva.kurochka@gmail.com>
2020-03-05 23:15:07 +0200

    add use form hook

---
+++ src/hooks/useForm.js
@@ -0,0 +1,41 @@
+import {useState} from 'react';
+
+const useForm = (initModel, onSubmit) => {
+    const [inputs, setInputs] = useState(initModel);
+
+    const handleChange = (name, event) => {
+        event.persist();
+        inputs.forEach((input) => {
+            if (input.name === name) {
+                input.value = event.target.value;
+                validateInput(input);
+            }
+        });
+        setInputs([...inputs]);
+    };
+
+    const validateInput = (input) => {
+        let alert = null;
+        input.validators && input.validators.forEach((validator) => {
+            alert = validator.isValid && !validator.isValid(input.value) ? validator.alert : null;
+        });
+        input.alert = alert;
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        inputs.forEach((input) => validateInput(input));
+        inputs.some(input => input.alert) ? setInputs([...inputs]) : onSubmit();
+    };
+
+    return {
+        inputs,
+        handleChange,
+        handleSubmit
+    };
+};
+
+export {
+    useForm
+};
+
